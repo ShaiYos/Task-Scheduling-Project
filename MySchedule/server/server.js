b9cbaf25084664c5bedc1ed8615ca26c80b36c2e
ShaiYos <shai44553@gmail.com>
2024-11-01 23:59:30 +0200

    added .env file to server side

--- MySchedule/server/server.js
+++ MySchedule/server/server.js
@@ -4,13 +4,15 @@ import mongoose from 'mongoose';
 import UserRoutes from './routes/UserRoutes.js'; 
 import taskRoutes from './routes/TaskRoutes.js';
 
+const dbURI = process.env.MONGODB_URI;
+
 const port = 3000; // Define the port on which the server will listen
 const app = express();
 
 app.use(cors()); // Enable CORS for all routes and origins
 app.use(express.json()); // Parses incoming JSON requests
 // Connect to MongoDB using the provided connection string (dbURI)
-const dbURI = 'mongodb+srv://admin:admin@cluster0.ebnx4.mongodb.net/TaskScheduling?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(dbURI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.log('Error connecting to MongoDB:', error));
